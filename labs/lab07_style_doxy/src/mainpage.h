052a2df12e8d03cb95669542656b150683966115
Bradley Botanel <botan008@umn.edu>
2020-02-14 14:55:21 -0600

    add(lab07): adding lab07 deliverables

---
+++ labs/lab07_style_doxy/src/mainpage.h
@@ -0,0 +1,9 @@
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Lab07_Style_Doxy
+ *
+ * By Bradley Botanel
+ * 
+ * This is documentation for Lab07.
+ *
+ */
--- labs/lab07_style_doxy/src/passenger.h
+++ labs/lab07_style_doxy/src/passenger.h
@@ -1,12 +1,39 @@
+/**
+ * @file passenger.h
+ *
+ * @copyright 2019 3081 Staff, All rights reserved.
+ */
+
 #ifndef PASSENGER_H
 #define PASSENGER_H
 
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <iostream>
 #include <string>
 
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief The main class for the passenger object.
+ * 
+ * 
+ */
 class Passenger { // : public Reporter {
  public:
+ /**
+  * @brief Constructor Passenger()
+  * 
+  * Methods: Update(), GetOnBus(), GetTotalWait(), 
+  * 
+  */
   Passenger(int = -1, std::string = "Nobody");
+  /**
+   * @brief Update function
+   * 
+   */
   void Update();
   void GetOnBus();
   int GetTotalWait() const;
@@ -14,6 +41,11 @@ class Passenger { // : public Reporter {
   int GetDestination() const;
   void Report() const;
  private:
+ /**
+  * @brief private
+  * 
+  * 
+  */
   std::string name_;
   int destination_stop_id_;
   int wait_at_stop_;
