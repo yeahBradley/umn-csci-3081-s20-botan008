c1cbdbfbbb650d7c1d9a4307023d92633a3d1676
Bradley Botanel <botan008@umn.edu>
2020-02-27 20:49:12 -0600

    Adding mainpage.h to src as lab10's src folder did not have one.

---
+++ project/src/mainpage.h
@@ -0,0 +1,17 @@
+#ifndef SRC_MAINPAGE_H_
+#define SRC_MAINPAGE_H_
+
+/**
+ * "Copyright 2020 Bradley Botanel"
+ * 
+ *  \mainpage My Personal Index Page
+ *
+ * \section intro_sec Lab07_Style_Doxy
+ *
+ * By Bradley Botanel
+ * 
+ * This is documentation for Lab07.
+ *
+ */
+
+#endif  // SRC_MAINPAGE_H_
